29624d820884b39083e5f26143a6c6d858c9b183
Lorenzo Spoleti <spolel@usi.ch>
2016-12-07 16:29:22 +0100

    + pointers on delete buttons + now size of canvas adapts to size of page when it's opened, for now if a resize happens you need to refresh

---
+++ public/js/room-init.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementById('canvas'),
+      x =  g.clientWidth,
+      y = g.clientHeight;
+  // alert(x + ' × ' + y);
+  g.width = x;
+  g.height = y;
+ });
