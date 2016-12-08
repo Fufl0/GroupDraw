59df112043c0dc53bbd9c175ce57f69877f1ccfc
SusySusay94 <ardigs@usi.ch>
2016-12-08 14:13:51 +0100

    Added socket part for some buttons

--- socket-server.js
+++ socket-server.js
@@ -3,11 +3,13 @@ module.exports = function(httpServer) {
     const io = require('socket.io').listen(httpServer);
 
     function joinRoom(socket, id) {
-        console.log(`A user joined room ${id}`)
+        console.log(`A user joined room ${id}`);
+        console.log("ip address: ", socket.request.connection.remoteAddress);
         socket.join(id);
     }
 
-    let roomHistories = {}
+    let roomHistories = {};
+    let roomUndoHistories = {};
 
     io.on("connection", function(socket) {
         let id;
@@ -18,8 +20,11 @@ module.exports = function(httpServer) {
 
             if (!roomHistories[id])
                 roomHistories[id] = [];
-            socket.emit("load-history", {
-                history: roomHistories[id]
+            if (!roomUndoHistories[id])
+                roomUndoHistories[id] = [];
+            socket.emit("load", {
+                history: roomHistories[id],
+                undohistory: roomUndoHistories[id]
             });
         });
         socket.on("clear", function() {
@@ -30,5 +35,17 @@ module.exports = function(httpServer) {
             roomHistories[id].push(message.stroke);
             socket.broadcast.to(id).emit("draw", message);
         });
+
+        socket.on("undo", function(message) {
+            roomHistories[id] = message.history;
+            roomUndoHistories[id] = message.undohistory;
+            socket.broadcast.to(id).emit("undo", message);
+        });
+
+        socket.on("redo", function(message) {
+            roomHistories[id] = message.history;
+            roomUndoHistories[id] = message.undohistory;
+            socket.broadcast.to(id).emit("undo", message);
+        })
     });
 }
