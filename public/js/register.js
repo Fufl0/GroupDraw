b85c7d2b357bfc49146fcae1bb3f752560d4a682
Thomas <thomas.d.p@hotmail.it>
2016-12-08 16:01:10 +0100

    + complete register check (fuck you alerts)

--- public/js/register.js
+++ public/js/register.js
@@ -11,29 +11,47 @@ var registerButton = window.document.getElementById('registerButton');
 
 registerButton.onclick = function btnSubmitOnClick(e) {
   console.log('submitBtn clicked');
-  if (username.value !== '' && !(username.value.toLowerCase().indexOf("guest") >= 0) ) {
-    if (password.value !== '') {
-      if (password.value === confirmPassword.value) {
-        var user = {
-          username: username.value,
-          password: password.value,
-        };
-        var r = new XMLHttpRequest();
-        r.open('POST', '/register');
-        r.setRequestHeader('Content-Type', 'application/json');
-        r.setRequestHeader('Accept', 'application/json');
-        r.onreadystatechange = function onReadyStateChange() {
-          if (r.readyState !== 4) return;
-          if (r.readyState === 4 && r.status === 201) {
-            alert("You succesfully registered, have fun :)");
-            window.location = '/welcome';
-          }
-          if (r.readyState === 4 && r.status === 500) {
-            alert("Username already in use!");
-          }
-        };
-        r.send(JSON.stringify(user));
+  if (username.value !== '') {
+    if ( !(username.value.toLowerCase().indexOf("guest") >= 0) ) {
+      if (password.value !== '') {
+        if (password.value === confirmPassword.value) {
+          var user = {
+            username: username.value,
+            password: password.value,
+          };
+          var r = new XMLHttpRequest();
+          r.open('POST', '/register');
+          r.setRequestHeader('Content-Type', 'application/json');
+          r.setRequestHeader('Accept', 'application/json');
+          r.onreadystatechange = function onReadyStateChange() {
+            if (r.readyState !== 4) return;
+            if (r.readyState === 4 && r.status === 201) {
+              window.location = '/welcome';
+            }
+            if (r.readyState === 4 && r.status === 500) {
+              document.getElementById("usernameLabel").setAttribute("data-error", "Username already in use");
+              $("#username").addClass("invalid");
+              $("#username").prop("aria-invalid", "true");
+            }
+          };
+          r.send(JSON.stringify(user));
+        } else {
+          $("#confirmPassword").addClass("invalid");
+          $("#confirmPassword").prop("aria-invalid", "true");
+        }
+      } else {
+        $("#password").addClass("invalid");
+        $("#password").prop("aria-invalid", "true");
       }
+    } else {
+      document.getElementById("usernameLabel").setAttribute("data-error", "Username can't contain 'guest' keyword");
+      $("#username").addClass("invalid");
+      $("#username").prop("aria-invalid", "true");
     }
+  } else {
+    document.getElementById("usernameLabel").setAttribute("data-error", "Username can't be empty");
+    $("#username").addClass("invalid");
+    $("#username").prop("aria-invalid", "true");
   }
+
 };
