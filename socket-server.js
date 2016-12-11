3c1617109795d5dae8733e824e6e3ad89c935b15
Thomas <thomas.d.p@hotmail.it>
2016-12-11 18:53:24 +0100

    + started working on socket for online-offline functionality

--- socket-server.js
+++ socket-server.js
@@ -1,6 +1,9 @@
 module.exports = function(httpServer) {
-
-    const io = require('socket.io').listen(httpServer);
+  const io = require('socket.io').listen(httpServer);
+	const mongoose = require('mongoose');
+	require('./models/User');
+	const Contact = mongoose.model('User');
+  const session = require('express-session');
 
     function joinRoom(socket, id) {
         console.log(`A user joined room ${id}`);
@@ -13,6 +16,27 @@ module.exports = function(httpServer) {
 
     io.on("connection", function(socket) {
         let id;
+
+        //USER
+        socket.on('online', function(id) {
+    			User.update({_id: req.session.user._id}, {status: 'online'}, function(err, n) {
+    				console.log(req.session.user.username + ' is online.');
+    				io.emit('online', _id);
+    			});
+    		});
+
+        socket.on('disconnect', function() {
+    			//if client was online
+    			if (_id) {
+    				CUser.update({_id: req.session.user._id}, {status : 'offline'}, function(err, n) {
+    					console.log(req.session.user.username + ' is offline');
+    					if (_id) io.emit('offline', _id);
+    				});
+    			}
+    		});
+
+
+        // ROOM
         socket.on("join", function(roomId) {
             id = roomId;
 
