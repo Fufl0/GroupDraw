429ad223b516fc815739c9d9816ada788ca121fb
SusySusay94 <ardigs@usi.ch>
2016-12-11 21:51:53 +0100

    Changed star brush and made the brush sizes more similar to each other

--- public/js/brushes.js
+++ public/js/brushes.js
@@ -3,23 +3,6 @@
 // this is an immediately-invoked function expression (IIFE)
 (function(){
 
-    //makes sure that val is within min and max
-    function within(val, min, max){
-        if (val < min) return min;
-        if (val > max) return max;
-        return val;
-    }
-
-    function sanityCheckOptionsStarBrush(options){
-        options.length = within(options.length, 15, 15);
-        options.angle = within(options.angle, 0, 180);
-        options.width = within(options.width, 1, 10);
-        options.opacity = within(options.opacity, 0, 1);
-        options.scale = within(options.scale, 0.1, 2);
-        return options;
-    }
-
-
     const penBrush = {
         name: 'Pen',
         draw: function(ctx, strokeStyle, x, y, r){
@@ -34,8 +17,8 @@
     const crossBrush = {
         name : "Cross",
 
-        draw : function (ctx, strokeStyle, x, y, radius){
-            let r = radius/4;
+        draw : function (ctx, strokeStyle, x, y, r){
+            let l = r / Math.PI;
 
             //do the actual drawing
             ctx.lineJoin = 'miter';
@@ -43,21 +26,20 @@
             ctx.save();
             ctx.translate(x, y);
             ctx.beginPath();
-            ctx.scale(r, r);
+            ctx.scale(l, l);
             ctx.strokeStyle = strokeStyle;
-            ctx.lineWidth = Math.ceil(r/10);
-            ctx.lineTo(0, (r/2));
-            ctx.lineTo(0, -(r/2));
+            ctx.lineWidth = Math.ceil(l/10);
+            ctx.lineTo(0, l);
+            ctx.lineTo(0, -l);
             ctx.closePath();
             ctx.stroke();
             ctx.beginPath();
-            ctx.lineTo((r/2), 0);
-            ctx.lineTo(-(r/2), 0);
+            ctx.lineTo(l, 0);
+            ctx.lineTo(-l, 0);
             ctx.closePath();
             ctx.stroke();
             ctx.restore();
         }
-
     };
 
 
@@ -72,10 +54,10 @@
             // ctx.scale(r);
             ctx.strokeStyle = strokeStyle;
             ctx.lineWidth = r/4;
-            ctx.lineTo(-l/2, l/2);
-            ctx.lineTo(l/2, l/2);
-            ctx.lineTo(l/2, -l/2);
-            ctx.lineTo(-l/2, -l/2);
+            ctx.lineTo(-l, l);
+            ctx.lineTo(l, l);
+            ctx.lineTo(l, -l);
+            ctx.lineTo(-l, -l);
             ctx.closePath();
             ctx.stroke();
             ctx.restore();
@@ -105,7 +87,7 @@
     const triangularBrush = {
         name : "Triangle",
         draw : function (ctx, strokeStyle, x, y, r) {
-            let l = r * Math.PI;
+            let l = r * 2 * Math.PI;
 
             ctx.lineJoin = 'mitter';
             ctx.save();
@@ -124,39 +106,26 @@
 
     const starBrush = {
         name : 'Star',
-        getOptions: function (r){
-            return {
-                length: 5,
-                angle: 0,
-                scale: r/25,
-                width: 1,
-            }
-        },
+
         draw : function (ctx, strokeStyle, x, y, r){
-
-            //check values
-            let options = this.getOptions(r);
-            options = sanityCheckOptionsStarBrush(options);
-
-            //do the actual drawing
+            let l = r * Math.PI;
             ctx.lineJoin = 'miter';
             ctx.lineCap = 'butt';
             ctx.save();
             ctx.translate(x, y);
             ctx.beginPath();
-            ctx.rotate(Math.PI / 180 * options.angle);
-            ctx.scale(options.scale, options.scale);
-            ctx.strokeStyle = options.color || strokeStyle;
-            ctx.lineWidth = options.width;
+            ctx.rotate(Math.PI / 180);
+            ctx.strokeStyle = strokeStyle;
+            ctx.lineWidth = r/4;
             for (let i = 5; i--;) {
-                ctx.lineTo(0, options.length);
-                ctx.translate(0, options.length);
+                ctx.lineTo(0, l);
+                ctx.translate(0, l);
                 ctx.rotate((Math.PI * 2 / 10));
-                ctx.lineTo(0, -options.length);
-                ctx.translate(0, -options.length);
+                ctx.lineTo(0, -l);
+                ctx.translate(0, -l);
                 ctx.rotate(-(Math.PI * 6 / 10));
             }
-            ctx.lineTo(0, options.length);
+            ctx.lineTo(0, l);
             ctx.closePath();
             ctx.stroke();
             ctx.restore();
