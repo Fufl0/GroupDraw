8411247203ca5bbe4b8eb749490aaf57b6142c0a
Thomas <thomas.d.p@hotmail.it>
2016-12-12 17:34:46 +0100

    + now the form in profile also updates the session

--- routes/profile/router.js
+++ routes/profile/router.js
@@ -26,6 +26,9 @@ router.modifyUser = function modifyUser(req, res) {
   if(!req.session.user) {
     return res.status(401).send();
   } else {
+    req.session.user.mood =req.body.mood;
+    req.session.user.status =req.body.status;
+    // TODO image ^
     User.update({username: req.body.username},
                 {mood: req.body.mood,
                   status: req.body.status,
