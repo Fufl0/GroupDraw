2bdcb20b2b5bbb1fcacd9c6d98f1a2b391e46ef7
Lorenzo Spoleti <spolel@usi.ch>
2016-12-12 19:08:58 +0100

    + you can set canvas width but it is not yet saved when you exit room + canvas bigger than screen is now scrollable + size setter in toolbar

--- public/js/room-init.js
+++ public/js/room-init.js
@@ -5,9 +5,22 @@ $(document).ready(function(){
       g = d.getElementById('canvas'),
       x =  g.clientWidth,
       y = g.clientHeight;
-  // alert(x + ' × ' + y);
-  g.width = x;
-  g.height = y;
+  // alert($(window).width() + ' × ' + $(window).height());
+
+  // g.width = x;
+  // g.height = y;
+
+  var size = document.getElementById('size').value.split("x");
+  var canvas = document.getElementById('canvas');
+  canvas.width = size[0]
+  canvas.height = size[1]
 
   $('.modal').modal();
  });
+
+document.getElementById('sizeSetterButton').onclick=function(){
+  var size = document.getElementById('size').value.split("x");
+  var canvas = document.getElementById('canvas');
+  canvas.width = size[0]
+  canvas.height = size[1]
+};
