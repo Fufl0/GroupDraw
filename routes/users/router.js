aa2c90d8eb5ebfdb311f29431414b94d567b2dc4
Thomas <thomas.d.p@hotmail.it>
2016-12-14 12:53:52 +0100

    + Small fix (forgot to remove commented lines)

--- routes/users/router.js
+++ routes/users/router.js
@@ -16,16 +16,15 @@ router.all('/', middleware.supportedMethods('GET, POST, DELETE, OPTIONS'));
 
 router.get('/list', function(req, res, next) {
   console.log("REC");
-  /*if (!req.session.user) {
+  if (!req.session.user) {
     res.status(401);
     res.redirect('/welcome');
-	} else {*/
-
+	} else {
     User.find(function(err, users) {
       if (err) return console.error(err);
       res.status(200).json(users);
     })
-//	}
+	}
 })
 
 router.get('/', function(req, res, next) {
