34bc2b56311278f8a9c2126be69d54547b7d84cd
Lorenzo Spoleti <spolel@usi.ch>
2016-12-16 11:41:18 +0100

    + now in the users screen colors change for the status (still have to put circles)

--- public/js/users.js
+++ public/js/users.js
@@ -1,3 +1,29 @@
+// this is for the colors in offline online
+
+window.onload = function() {
+  var x = document.getElementsByClassName("statusLabel");
+  for(var i in x){
+    if(x[i].innerHTML.includes("offline")){
+      x[i].style = "color:red;"
+    }
+    if(x[i].innerHTML.includes("online")){
+      x[i].style = "color:green;"
+    }
+    if(x[i].innerHTML.includes("ready to draw")){
+      x[i].style = "color:green;"
+    }
+    if(x[i].innerHTML.includes("away")){
+      x[i].style = "color:yellow;"
+    }
+    if(x[i].innerHTML.includes("busy")){
+      x[i].style = "color:red;"
+    }
+
+  }
+
+};
+
+
 
   const socket = io();
   socket.on('reload', function(){
